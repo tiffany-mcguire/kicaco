1edd5fb62d61cc0a7ec8fe24dd67822ae5a22713
Tiffany McGuire <tmac@Tiffanys-MacBook-Pro.local>
2025-06-25 22:18:21 -0700

    Replace birthday party event with fresh event to fix UI caching
    - Deleted problematic 'Birthday Party' event that was showing as 'Alex's Birthday Party' in UI
    - Created new 'Sarah's Birthday' event with same details but fresh identity
    - This should resolve any caching issues causing incorrect display
    - Event clearly shows it's Sarah's birthday with Alex attending

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ed3775ef'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9cm4ehebs3o"
+    "revision": "0.vdiejpu42lo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
